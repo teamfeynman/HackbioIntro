ade6235b8d974e72a21ca22d01aca1506cf966c1
HabenTick <92265936+HabenTick@users.noreply.github.com>
2021-10-12 11:03:31 +0200

    Add files via upload

---
+++ Habeneheir.Tickheir_HackBio.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(void)
+{
+	cout.width(24);
+    cout<<"Name:   Habeneheir H. Tickheir" << endl;
+    cout.width(24);
+    cout<<"Email:  habentickheir.ht@gmail.com"<< endl;
+    cout.width(24);
+    cout<<"Slack username:  @HabenTick" <<endl;
+    cout<<"Biostack:  Science communication" <<endl;
+    return 0;
+}
