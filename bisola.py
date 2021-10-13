206f1290857d305b45489852c0e1fc66aa160d3d
Filani-Bisola <90179682+Filani-Bisola@users.noreply.github.com>
2021-10-12 23:00:33 -0700

    Bisola.py
    This first assignment

---
+++ bisola.py
@@ -0,0 +1,17 @@
+# welcome world
+# My first assignment
+This is my first resipository
+#!/usr/bin/env python3
+# Prints out my name, email, language, biostack and slack
+
+
+def personal_details().
+  name = "Filani Bisola"
+  email = "filabis1@gmail.com"
+  language = "python'
+  biostack = "Vaccine informatics"
+  slack = "@Bisola"
+  print("{},{},{},{},{}".format(name,email,language,biostack,slack))
+  
+  
+  personal_details()
