98947217f8d2ae276bd02acd96ecd332e175919e
LamyaaMohammad <92541043+LamyaaMohammad@users.noreply.github.com>
2021-10-15 14:51:32 +0200

    Add files via upload

---
+++ Lamyaa.py
@@ -0,0 +1,5 @@
+name = input("Enter your name: ")
+email = input("Enter your email: ")
+slack = input("Enter your slack username: ")
+biostack= input("Enter your biostack: ")
+print ( "Hello, my name is "+ name+"," " my email is "+ email+"," + "@" +slack + " is my slack username," +" and I choosed "+ biostack +" as my biostack!")
