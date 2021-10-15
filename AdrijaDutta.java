f8fc1f34d52a6f093629f58b2cde07b648a59251
Adrija Dutta <92315643+ohdrija@users.noreply.github.com>
2021-10-15 18:08:00 +0530

    Add files via upload

---
+++ AdrijaDutta.java
@@ -0,0 +1,16 @@
+package com.srccodes.example ;
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		
+			System.out.println ("Adrija Dutta");
+			System.out.println("adrijadutta3@gmail.com");
+			System.out.println("@ohdrija");
+			System.out.println("Drug Discovery");
+		
+		
+	}
+
+}
