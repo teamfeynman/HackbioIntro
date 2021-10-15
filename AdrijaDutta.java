0174cee97460dd0751dbaa2cca3007f5f461f47a
Adrija Dutta <adrijadutta3@gmail.com>
2021-10-15 19:35:40 +0530

    removed blank lines

--- AdrijaDutta.java
+++ AdrijaDutta.java
@@ -2,15 +2,10 @@ package com.srccodes.example ;
 public class Main {
 
 	public static void main(String[] args) {
-		
-		
-		
 			System.out.println ("Adrija Dutta");
 			System.out.println("adrijadutta3@gmail.com");
 			System.out.println("@ohdrija");
-			System.out.println("Drug Discovery");
-		
-		
+			System.out.println("Drug Discovery");	
 	}
 
 }
