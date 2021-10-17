970680d88ba5b0228a83ea29da7e14bb323e4ac6
Mofy-foluwa <92272290+Mofy-foluwa@users.noreply.github.com>
2021-10-17 00:40:40 -1100

    Create mofy.R

---
+++ mofy.R
@@ -0,0 +1,12 @@
+personal_info <- function (){
+ Name = "Oluwadamilola Olutayo-David"
+  Email = "olutayordavemofyd@gmail.com"
+   Slack_username = @mofy
+   Biostack = "Genomics
+   
+   cat(paste('/n', Name, ",",
+             Email, ",",
+             Slack_username, ",",
+             Biostack, '/n'))
+    }
+    personal_info()
